c9c63a6a0d1dbad9bf249e538c771dad3921856a
DESKTOP-BA7DGJE\Usuario <arielgolafer@gmail.com>
2021-10-27 14:18:26 -0300

    Monted server, problem with webpack 'temporaly solved', and make NavMenu component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import './App.css';
 import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
+import NavMenu from './components/NavMenu/NavMenu';
+
 function App() {
   return (
     <Router>
-      
+
+      <NavMenu />
+
     </Router>
   );
 }
